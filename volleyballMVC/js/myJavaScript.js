b20f49a19f30055caf6a810b8a129c2f6fa5c42e
parsa <f_lamee2002@yahoo.com>
2016-10-28 20:00:26 -0700

    changed the background every 5 seconds

---
+++ volleyballMVC/js/myJavaScript.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    var header = $('header');
+    var backgrounds = [
+      'url(img/logos/shirt1.jpg)',
+      'url(img/header-bg.jpg)'];
+
+    var current = 0;
+    //$("body").css({background : "url(../img/bgr.png)", background-repeat: 'no-repeat'}); text-align: center;
+    //$('body').css('background', '#000 url(' + backgrounds[bgCounter] + ') no-repeat');
+    function nextBackground() {
+        header.css('background', backgrounds[current = ++current % backgrounds.length] + 'no-repeat');
+        header.css('background-size', 'cover');
+        header.css('background-attachment', 'scroll');
+        header.css('text-align', 'center');
+        header.css('animation', 'fadeIn 2s');
+        
+        setTimeout(nextBackground, 5000);
+    }
+    setTimeout(nextBackground, 5000);
+    header.css('background', backgrounds[0]);
+});
